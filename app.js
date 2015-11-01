88005bba20ecea51c6d426f3459ae78c41276bbb
nineonine <mail4chemik@gmail.com>
2015-11-01 00:40:08 -0700

    add User REST routes

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var db = require('./model/db');
 
+var db = require('./model/db'),
+    user = require('./model/users');
+
+// -------- Routes ----------
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
