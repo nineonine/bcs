df65fa78cfe438fffc23e5797eeb07c314ec4e16
nineonine <mail4chemik@gmail.com>
2015-11-20 21:11:07 -0800

    beta version deployment

--- model/db.js
+++ model/db.js
@@ -1,2 +1,3 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/bcs');
+mongoose.connect('mongodb://bcsadmin:letmein@ds057234.mongolab.com:57234/bcs')
+//mongoose.connect('mongodb://localhost/bcs');
